ca708cd74942cfb7cb0026bdfd74d5a1cd07fa89
Sharifjon <sharifboymuminov@gmail.com>
2024-07-03 19:50:25 +0500

    first commit

---
+++ lib/utils/app_text_style.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:to_do_app/utils/app_colors.dart';
+
+class AppTextStyle {
+  // SeoulNamsan
+  static TextStyle seoulNamsanThin = TextStyle(
+    fontFamily: "SeoulNamsan",
+    fontWeight: FontWeight.w200,
+    fontSize: 14.sp,
+    color: AppColors.black,
+  );
+  static TextStyle seoulNamsanLight = TextStyle(
+    fontFamily: "SeoulNamsan",
+    fontWeight: FontWeight.w300,
+    fontSize: 14.sp,
+    color: AppColors.black,
+  );
+  static TextStyle seoulNamsanRegular = TextStyle(
+    fontFamily: "SeoulNamsan",
+    fontWeight: FontWeight.w400,
+    fontSize: 16.sp,
+    color: AppColors.black,
+  );
+  static TextStyle seoulNamsanMedium = TextStyle(
+    fontFamily: "SeoulNamsan",
+    fontWeight: FontWeight.w500,
+    fontSize: 16.sp,
+    color: AppColors.black,
+  );
+
+  static TextStyle seoulNamsanSemiBold = TextStyle(
+    fontFamily: "SeoulNamsan",
+    fontWeight: FontWeight.w600,
+    fontSize: 16.sp,
+    color: AppColors.black,
+  );
+
+  static TextStyle seoulNamsanBold = TextStyle(
+    fontFamily: "SeoulNamsan",
+    fontWeight: FontWeight.w700,
+    fontSize: 16.sp,
+    color: AppColors.black,
+  );
+
+  static TextStyle seoulNamsanBlack = TextStyle(
+    fontFamily: "SeoulNamsan",
+    fontWeight: FontWeight.w800,
+    fontSize: 16.sp,
+    color: AppColors.black,
+  );
+
+  // Nunito
+  static TextStyle nunitoThin = TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w200,
+    fontSize: 14.sp,
+    color: AppColors.black,
+  );
+  static TextStyle nunitoLight = TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w300,
+    fontSize: 14.sp,
+    color: AppColors.black,
+  );
+  static TextStyle nunitoRegular = TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w400,
+    fontSize: 16.sp,
+    color: AppColors.black,
+  );
+  static TextStyle nunitoMedium = TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w500,
+    fontSize: 16.sp,
+    color: AppColors.black,
+  );
+
+  static TextStyle nunitoSemiBold = TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w600,
+    fontSize: 16.sp,
+    color: AppColors.black,
+  );
+
+  static TextStyle nunitoBold = TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w700,
+    fontSize: 16.w,
+    color: AppColors.black,
+  );
+
+  static TextStyle nunitoBlack = TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w800,
+    fontSize: 16.sp,
+    color: AppColors.black,
+  );
+}
